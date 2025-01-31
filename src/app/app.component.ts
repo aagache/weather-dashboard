061ef810b9f422a52d701b2e1f71fd8fe22485cd
Andrei Agache <andrei.agache@it-teams.com>
2025-01-31 11:41:06 +0200

    adds angular material; updates the favicon; creates the main view components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,17 +2,17 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LocationService } from './services/location/location.service';
 import { WeatherService } from './services/weather/weather.service';
+import { DashboardComponent } from './view/dashboard/dashboard.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, DashboardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'weather-dashboard';
-  location: any;
 
   constructor(
     private locationService: LocationService,
@@ -24,7 +24,6 @@ export class AppComponent {
    */
   ngOnInit(): void {
     this.locationService.getLocation('London').subscribe((data: any) => {
-      this.location = data;
       console.log('1', data)
 
       this.weatherService.getCurrentWeather(data[0].lat, data[0].lon).subscribe(data => {
