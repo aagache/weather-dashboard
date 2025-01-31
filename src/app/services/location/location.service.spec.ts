6953eb2a6dc231933d6dfa5868ad6b710ec2b7c6
Andrei Agache <andrei.agache@it-teams.com>
2025-01-31 10:24:22 +0200

    tests the new service and fixes the other failing tests

--- src/app/services/location/location.service.spec.ts
+++ src/app/services/location/location.service.spec.ts
@@ -1,16 +1,45 @@
 import { TestBed } from '@angular/core/testing';
-
 import { LocationService } from './location.service';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { LocationType } from '../../models/location';
+import { environment } from '../../../../environments/environment';
 
 describe('LocationService', () => {
   let service: LocationService;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [LocationService],
+    });
+    
     service = TestBed.inject(LocationService);
+    httpTestingController = TestBed.inject(HttpTestingController);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should fetch location data for a given place', (done: DoneFn) => {
+    const place = 'Oradea';
+
+    const mockLocation: LocationType = {
+      display_name: 'Oradea, Romania',
+      lat: 40.123, 
+      lon: -40.456,
+    };
+
+    service.getLocation(place).subscribe(data => { 
+      expect(mockLocation).toEqual(data);
+      done();
+     });
+
+     const req = httpTestingController.expectOne(
+      `${environment.locationApi}search?q=${place}&format=json`
+    );
+  
+    req.flush(mockLocation);
   });
 });
